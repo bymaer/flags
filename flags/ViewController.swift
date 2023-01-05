aaa3cbd52e5f2703429ab5859d6455343c25035b
bymaer <37290594+bymaer@users.noreply.github.com>
2023-01-05 22:03:58 +0100

    first alpha version

--- flags/ViewController.swift
+++ flags/ViewController.swift
@@ -9,11 +9,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    @IBAction func button1WasPressed(_ sender: UIButton) {
+//        var title: String
+        
+        let ok = UIAlertAction(title: "OK", style: .default)
+        
+        if answers == 10 {
+            let finalAlert = UIAlertController(title: "Game over", message: "Your final score is \(score)" , preferredStyle: .alert)
+            self.present(finalAlert, animated: true)
+        }
+        
+        if sender.imageView?.image == UIImage(named: countries[correctAnswer]) {
+            title = "Correct"
+            let correctAlert = UIAlertController(title: "Correct", message: "Congratulations. You are answered right!", preferredStyle: .alert)
+            self.present(correctAlert, animated: true, completion: askQuestion)
+            correctAlert.addAction(ok)
+            self.score += 1
+        } else {
+            title = "Wrong"
+            self.score -= 1
+            let wrongAlert = UIAlertController(title: "Oops", message: "Sorry. You are answered wrong! That’s the flag of \(countries[sender.tag].uppercased())", preferredStyle: .alert)
+            self.present(wrongAlert, animated: true, completion: askQuestion)
+            wrongAlert.addAction(ok)
+        }
+        
+        answers += 1
+    }
+    
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    var answers = 0
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("loaded")
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+    askQuestion()
     }
 
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = "Your score is \(score). Where is: \(countries[correctAnswer].uppercased())"
+    }
+    
 
 }
-
