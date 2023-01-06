9a7a0ac27d2186d66130ae7d27ee3a0972e8f0e2
bymaer <37290594+bymaer@users.noreply.github.com>
2023-01-06 21:33:00 +0100

    minor bug fix

--- flags/ViewController.swift
+++ flags/ViewController.swift
@@ -8,54 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
-    @IBAction func button1WasPressed(_ sender: UIButton) {
-//        var title: String
-        
-        let ok = UIAlertAction(title: "OK", style: .default)
-        
-        if answers == 10 {
-            let finalAlert = UIAlertController(title: "Game over", message: "Your final score is \(score)" , preferredStyle: .alert)
-            self.present(finalAlert, animated: true)
-        }
-        
-        if sender.imageView?.image == UIImage(named: countries[correctAnswer]) {
-            title = "Correct"
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                self.navigationController!.navigationBar.backgroundColor = .white
-            }
-            navigationController!.navigationBar.backgroundColor = .green
-            let correctAlert = UIAlertController(title: "Correct", message: "Congratulations. You are answered right!", preferredStyle: .alert)
-            self.present(correctAlert, animated: true, completion: askQuestion)
-            correctAlert.addAction(ok)
-            self.score += 1
-        } else {
-            title = "Wrong"
-            self.score -= 1
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                self.navigationController!.navigationBar.backgroundColor = .white
-            }
-            navigationController!.navigationBar.backgroundColor = .red
-            let wrongAlert = UIAlertController(title: "Oops", message: "Sorry. You are answered wrong! That’s the flag of \(countries[sender.tag].uppercased())", preferredStyle: .alert)
-            self.present(wrongAlert, animated: true, completion: askQuestion)
-            wrongAlert.addAction(ok)
-        }
-        
-        answers += 1
-    }
-    
-    
+    var answers = 0
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
-    var answers = 0
     
+    let ok = UIAlertAction(title: "OK", style: .default)
+    
+    @IBAction func button1WasPressed(_ sender: UIButton) {
+        
+        if sender.imageView?.image == UIImage(named: countries[correctAnswer]) {
+            self.score += 1
+            answers += 1
+            title = "Correct"
+            navigationController!.navigationBar.backgroundColor = .green
+            if answers <= 9 {
+                let correctAlert = UIAlertController(title: "Correct", message: "Congratulations. You answered correctly", preferredStyle: .alert)
+                self.present(correctAlert, animated: true)
+                correctAlert.addAction(ok)
+            } else {
+                let restartGameButtonAlert = UIAlertAction(title: "Restart game", style: .default)
+                let finalAlert = UIAlertController(title: "Game over", message: "Your final score is \(score).", preferredStyle: .alert)
+                finalAlert.addAction(restartGameButtonAlert)
+                self.present(finalAlert, animated: true)
+                answers = 0
+                score = 0
+            }
+        } else {
+            title = "Wrong"
+            self.score -= 1
+            answers += 1
+            navigationController!.navigationBar.backgroundColor = .red
+            if answers <= 9 {
+                let wrongAlert = UIAlertController(title: "Oops", message: "Sorry. You answered wrong! That’s the flag of \(countries[sender.tag].uppercased())", preferredStyle: .alert)
+                self.present(wrongAlert, animated: true)
+                wrongAlert.addAction(ok)
+            } else {
+                let restartGameButtonAlert = UIAlertAction(title: "Restart game", style: .default)
+                let finalAlert = UIAlertController(title: "Game over", message: "Your final score is \(score).", preferredStyle: .alert)
+                finalAlert.addAction(restartGameButtonAlert)
+                self.present(finalAlert, animated: true)
+                answers = 0
+                score = 0
+            }
+            
+        }
+        askQuestion()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.navigationController!.navigationBar.backgroundColor = .white
+        }
 
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,15 +73,14 @@ class ViewController: UIViewController {
         
         
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "My score", style: .plain, target: self, action: #selector(heyho))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "My score", style: .plain, target: self, action: #selector(showScore))
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
         button3.layer.borderWidth = 1
         
-        
-    askQuestion()
+        askQuestion()
     }
-
+    
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
@@ -83,11 +92,10 @@ class ViewController: UIViewController {
         title = "Where is: \(countries[correctAnswer].uppercased())?"
     }
     
-    @objc func heyho() {
-        let scoreAlert = UIAlertController(title: "Score", message: "Your score is \(score)", preferredStyle: .alert)
+    @objc func showScore() {
+        let scoreAlert = UIAlertController(title: "Score", message: "Your score is \(score). \n Attempts: \(answers)", preferredStyle: .alert)
         self.present(scoreAlert, animated: true, completion: nil)
-        let ok = UIAlertAction(title: "OK", style: .default)
         scoreAlert.addAction(ok)
     }
-
+    
 }
